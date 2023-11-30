46e496c136a403649c917f34900876a671fe040b
Veluru Lokesh <velokesh@deloitte.com>
2023-11-30 15:55:02 +0530

    Add Test layer for Application

---
+++ Coupon.Application.Tests/GetRecommendedCouponsQueryHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿#region References
+using AutoFixture;
+using AutoFixture.NUnit3;
+using Coupon.Application.Interfaces;
+using Coupon.Application.Queries;
+using Coupon.Application.Tests;
+using Coupon.Domain.Models;
+using Coupon.Infrastructure.Repositories.DTO;
+using Coupon.Infrastructure.Repositories.Interfaces;
+using Moq;
+#endregion
+
+namespace Coupon.Api.Tests
+{
+    public class GetRecommendedCouponsQueryHandlerTests
+    {
+        #region Get Recommended Coupons Query Handler Tests
+        [Test, CustomAutoData()]
+        public async Task GetRecommendedCouponsQueryHandler_ValidInput_ShouldReturnSuccessResponse(
+            Fixture fixture,
+            [Frozen] ICouponOperation couponOperation,
+            GetRecommendedCouponsQueryHandler getRecommendedCouponsQueryHandler)
+        {
+            var request = fixture
+                .Build<RecommendedCouponDTO>()
+                .Create();
+
+            var couponsResponse = fixture.Build<CouponDTO>()
+                .CreateMany(2);
+
+            Mock.Get(couponOperation)
+                .Setup(x => x.GetCoupons())
+                .ReturnsAsync(couponsResponse);
+
+            var response = await getRecommendedCouponsQueryHandler
+                .ExecuteQuery(request);
+
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response.Coupons, Has.Count.EqualTo(couponsResponse.Count()));
+        }
+
+        #endregion
+    }
+}
